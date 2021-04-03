47d5019a7d25bad3c4473dacd04dc3d224667f2a
Pau <frikeldon@gmail.com>
2021-04-03 13:30:31 +0200

    Componente Label

---
+++ component/label/label.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  props: {
+    /** Representa Label como requerido. */
+    required: { type: Boolean, default: false },
+    /** Representa Label como deshabilitado. */
+    disabled: { type: Boolean, default: false }
+  }
+}
+</script>
+
+<template>
+  <label :class="{ required, disabled }">
+    <!-- @slot Contenido de Label. -->
+    <slot />
+  </label>
+</template>
+
+<style lang="less" scoped src="./label.less"></style>
+
+<docs>
+<fura-label v-text="'I\'m a Label'" />
+</docs>
+
+<docs>
+<fura-label
+  required
+  v-text="'I\'m a required Label'"
+/>
+</docs>
+
+<docs>
+<fura-label
+  disabled
+  v-text="'I\'m a disabled Label'"
+/>
+</docs>
+
+<docs>
+<fura-label
+  required
+  disabled
+  v-text="'I\'m a required and disabled Label'"
+/>
+</docs>
